49d2f474659c3d90d6025bb5c72dc44cd7ca9521
Suraj Aglave <aglavesuraj96@gmail.com>
2024-09-29 11:04:53 +0530

    change

--- helloworld.c
+++ helloworld.c
@@ -1,10 +1,6 @@
-#include <stdio.h>
-int main() 
+#include<stdio.h>
+int main()
 {
-    int a, b, c;
-    a=10;
-    b=20;
-    c=a+b;
-    printf("%d"c);
+    printf("HELLO WORLD");
     return 0;
 }
