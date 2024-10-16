77452a03f023e900b937bfd3b450a67d2e058182
Suraj Aglave <aglavesuraj96@gmail.com>
2024-10-16 15:57:24 +0530

    Create Subtraction.c

---
+++ Subtraction.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a, b, sum;
+    printf("Enter value of a :");
+    scanf("%d",&a);
+
+    printf("Enter value of b :");
+    scanf("%d",&b);
+
+    sum= a - b;
+    printf("Sum of a and b is : %d",sum);
+return 0;
+}
