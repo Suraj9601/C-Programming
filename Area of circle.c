2a8fece474e5b061cae15df9ff7927b70ca5f6d6
Suraj Aglave <aglavesuraj96@gmail.com>
2024-10-24 14:25:20 +0530

    Update Area of circle.c

--- Area of circle.c
+++ Area of circle.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
-int main()
-{
-    
-}
 
+int main() {
+    int a, b, c, largest;
+    
+    // Input three numbers
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+    
+    // Using ternary operator to find the largest
+    largest = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+    
+    // Output the result
+    printf("The greatest number is: %d\n", largest);
+    
+    return 0;
+}
