4d0cb5ef09911d2d7be0a8181b87d0aa2f0e7936
Suraj Aglave <aglavesuraj96@gmail.com>
2024-10-25 20:23:57 +0530

    Update Product of No.c

---
+++ Product of No.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() 
+{
+    int a,b,product;
+    printf("Enter the number a :");
+    scanf("%d",&a);
+
+    printf("Enter the number b :");
+    scanf("%d",&b);
+
+    product = a * b;
+
+    printf
+    ("The product of %d and %d is %d",a,b,product);
+
+    return 0;
+}
