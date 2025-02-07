4b76391de9af50a46d24c78597b71a3f84566b35
Suraj Aglave <aglavesuraj96@gmail.com>
2025-02-07 16:17:52 +0530

    Create Greatest no.c

---
+++ Greatest no.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() 
+{
+    float num1,num2,num3;
+    float greatest_no;
+
+    printf("Enter value of num1, num2 and num3 :");
+    scanf("%f%f%f",&num1,&num2,&num3);
+
+    greatest_no = num1
+
+    printf("Greatest number is %f",num1,num2,num3,greatest_no);
+
+    return 0;
+}
